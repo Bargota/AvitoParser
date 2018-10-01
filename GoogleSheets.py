8e83eaaf6acc6e572e1c5a7a9174457be9395572
0dyvan <v.orlov.geo@gmail.com>
2018-10-01 20:42:02 +0300

    GooglSheet
    Простой пример работы с GoogleSheet не связанный с работой приложения

--- GoogleSheets.py
+++ GoogleSheets.py
@@ -1,6 +1,7 @@
 import httplib2
 import apiclient.discovery
 from oauth2client.service_account import ServiceAccountCredentials
+import pyperclip
 
 
 CREDENTIALS_FILE = 'AvitoParser-fa42c491fb6e.json'  # имя файла с закрытым ключом
@@ -18,12 +19,17 @@ spreadsheet = service.spreadsheets().create(body = {
                                'gridProperties': {'rowCount': 8, 'columnCount': 5}}}]
 }).execute()
 
+#Доступ к фйлу через Google Drive API
 driveService = apiclient.discovery.build('drive', 'v3', http = httpAuth)
 shareRes = driveService.permissions().create(
     fileId = spreadsheet['spreadsheetId'],
-    body = {'type': 'anyone', 'role': 'reader'},  # доступ на чтение кому угодно
+    #body = {'type': 'user', 'role': 'writer', 'emailAddress': 'v.orlov.geo@gmail.com'}, 
+    body={'type': 'anyone', 'role': 'reader'}, # доступ на чтение кому угодно
     fields = 'id'
 ).execute()
+
+print(spreadsheet['spreadsheetId'])
+pyperclip.copy(spreadsheet['spreadsheetId'])
 
 results = service.spreadsheets().values().batchUpdate(spreadsheetId = spreadsheet['spreadsheetId'], body = {
     "valueInputOption": "USER_ENTERED",
@@ -34,6 +40,38 @@ results = service.spreadsheets().values().batchUpdate(spreadsheetId = spreadshee
 
         {"range": "Сие есть название листа!D5:E6",
          "majorDimension": "COLUMNS",  # сначала заполнять столбцы, затем ряды (т.е. самые внутренние списки в values - это столбцы)
-         "values": [["This is D5", "This is D6"], ["This is E5", "=5+5"]]}
+         "values": [["This is D5", "This is D6"], ["This is E5", "=5+5"]]},
+		 {"range": "Сие есть название листа!A1:A2",
+         "majorDimension": "COLUMNS",  # сначала заполнять столбцы, затем ряды (т.е. самые внутренние списки в values - это столбцы)
+         "values": [["=E6+12","123"]]}
     ]
 }).execute()
+
+print('123')
+
+#results = service.spreadsheets().values().batchUpdate(spreadsheetId = spreadsheet['spreadsheetId'], body = {
+#    "valueInputOption": "USER_ENTERED",
+#    "data": [
+#        {"range": "Сие есть название листа!A1:A2",
+#         "majorDimension": "ROWS",     # сначала заполнять ряды, затем столбцы (т.е. самые внутренние списки в values - это ряды)
+#         "values": [["This is B3", "This is C2"]]}]}).execute()
+
+#request = service.spreadsheets().values().append(spreadsheetId='1Di7CgLURGb1rNLvyC8UVTgFZmh6LKBmFGtw9KlsHr6A',
+#												 range="Сие есть название листа!A1",
+#												 valueInputOption='',
+#												 insertDataOption='', 
+#												 body='123').execute()
+
+
+#Добавление данных в ячейку
+results = service.spreadsheets().values().batchUpdate(spreadsheetId = '1RvgTYKbK89KQpqN6nW7w95yBX7BlGXbr3ZjHNgdnQ_o', body = {
+    "valueInputOption": "USER_ENTERED",
+    "data": [
+        
+		 {"range": "Сие есть название листа!A5",
+         "majorDimension": "COLUMNS",  # сначала заполнять столбцы, затем ряды (т.е. самые внутренние списки в values - это столбцы)
+         "values": [["5"]]}
+    ]
+}).execute()
+
+request = service.spreadsheets().values().update(spreadsheetId='1RvgTYKbK89KQpqN6nW7w95yBX7BlGXbr3ZjHNgdnQ_o', range='A6', valueInputOption='USER_ENTERED', body=['6']).execute()
