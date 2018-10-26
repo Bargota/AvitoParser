0f8c6b9078a9d0fdf31e09726b0bdf9de29a7b41
0dyvan <v.orlov.geo@gmail.com>
2018-10-26 17:44:53 +0300

    перед слиянием с главной веткой
    поиск улицы
    поиск номера дома

--- YearOfConstruction/main_for_year.py
+++ YearOfConstruction/main_for_year.py
@@ -4,6 +4,154 @@
 import re
 import sys
 
+class FindAddres:
+	def __init__(self):
+		self.street_name = ''
+		self.place_in_string = 0
+		self.house_number = ''
+
+
+
+def find_addres(path_address,path_street):
+	all_streets = open(path_street,'r')
+	address = open(path_address,'r')
+
+	
+	count1=0
+	count2=0
+	count3=0
+	list=[]
+	for addres in address:
+		addres=re.sub(r'\s+',' ',addres)
+		addres=addres.lower().strip()
+		count1+=1		
+		my_addres = FindAddres()
+		my_addres.street_name, my_addres.place_in_string=find_regular_street(addres,path_street)
+		my_addres.house_number=find_regular_house(addres,my_addres.place_in_string)
+		
+		list.append({'street':my_addres.street_name,'house':my_addres.house_number})
+		#if count1%20==0:
+		#	print(str(my_addres.place_in_string)+' '+my_addres.street_name+' '+addres[my_addres.place_in_string:])
+
+		#print(str(count1)+' '+my_addres.street_name+' '+addres)
+	return list
+
+
+def find_regular_street(addres,path_street):
+	all_streets = open(path_street,'r')
+	list_streets=[]
+	flag_uncertainty=0
+	flag_not_found=1
+	for street_name in all_streets:
+		list_streets.append(street_name)
+
+	tmp=re.findall(r',? ?\bул(?:иц[ае])?[ \.][^,]{3,},?', addres)
+	tmp2=re.findall(r',?[^,]+ \bул(?:иц[ае])?,?', addres)
+	if len(tmp)==0 and len(tmp2)==0:
+		previously_found_street=addres			
+	else:			
+		if len(tmp2)==0:
+			previously_found_street = StreetNameRemoveExcess(tmp[0])
+				
+		if len(tmp)==0:
+			previously_found_street = StreetNameRemoveExcess(tmp2[0])
+	if len(tmp)!=0 and len(tmp2)!=0:
+		previously_found_street=tmp[0]
+		flag_uncertainty=1
+				
+	for street in list_streets:
+			if previously_found_street.find(street.strip().lower())!=-1:					
+				found_street=street.strip()
+				flag_not_found=0
+				break
+
+	if flag_uncertainty:
+		if flag_not_found:
+			for street in list_streets:
+				if tmp2[0].find(street.strip().lower())!=-1:					
+						found_street=street.strip()
+						break
+
+	place = addres.find(found_street.lower())
+	if place!=-1:
+		place+=len(found_street)
+		
+	if place>len(addres) or place==-1:
+		print('Название улицы распозналось не корректно, суммарная длина больше длины адреса')
+		place=0
+	
+	return found_street,place
+
+def find_regular_house(addres,place):
+	"""Search for the number of house in addres"""
+		
+	
+	
+		
+	addres=re.sub(r'\s+',' ',addres)
+	addres=addres.lower().strip()
+	
+
+	regular = re.compile(r',? д?(?:ом)? ?\d{1,4}/?\\?\w{0,4},?')
+	tmp=regular.search(addres,place)
+	#tmp2=re.findall(r',?[^,]+ ул\w{3},?', addres)
+	if tmp!=None:
+		found_house = re.sub(r', ?','',addres[tmp.regs[0][0]:tmp.regs[0][1]])
+		found_house = re.sub(r' ?д?(?:ом)?','',found_house)
+	else:
+		print('номер дома не найден')
+		found_house=str(0)
+
+	return found_house
+	#if len(tmp)!=0 :
+	#	previously_found_house=tmp		
+	#print (str(count1)+' '+previously_found_house)		
+			
+
+	
+	
+
+def StreetNameRemoveExcess(name_street):
+	'''Delete from street name word 'street' '''
+	
+	name_street = re.sub(r'[ \.,]\bул(?:иц[ае])?[ \.,]','',name_street)
+	
+	name_street = re.sub(r', ?','',name_street)
+	
+	return name_street
+
+
+
+
+		
+
+
+
+
+#path_file = 'C:\\v.orlov\\Programm\\python\\AvitoParser\\YearOfConstruction\\address.txt'
+#path_all_streets='C:\\v.orlov\\Programm\\python\\AvitoParser\\YearOfConstruction\\streets_list.txt'
+#file = open(path_file,'r')
+#all_streets = open(path_all_streets,'r')
+
+main_path=sys.path[0]
+file_name_address = 'address.txt'
+file_name_streets='streets_list.txt'
+#address = open(main_path+'\\'+file_name_address,'r')
+#streets = open(main_path+'\\'+file_name_streets,'r')
+
+
+#find_regular_word(path_file)
+#find_street_name_from_file(main_path+'\\'+file_name_address,main_path+'\\'+file_name_streets)
+
+#find_regular_street(main_path+'\\'+file_name_address,main_path+'\\'+file_name_streets)
+#find_regular_house(main_path+'\\'+file_name_address)
+
+find_addres(main_path+'\\'+file_name_address,main_path+'\\'+file_name_streets)
+print('finish')
+input()
+
+#---------------------------------------------------------------------------------------------------
+
 def find_regular_word(file):
 	"""Search for the word 'street' in addres"""
 
@@ -28,43 +176,6 @@ def find_regular_word(file):
 	print(str(count2))
 	print(str(count))
 	print(str(count1))
-
-def find_regular_street(path_address,path_street):
-	all_streets = open(path_street,'r')
-	address = open(path_address,'r')
-
-	list_streets=[]
-	for street_name in all_streets:
-		list_streets.append(street_name)
-
-	for addres in address:
-		addres=re.sub(r'\s+',' ',addres)
-		addres=addres.lower().strip()
-
-
-		#for street in list_streets:
-
-		#	if addres.find(street)!=-1:
-		#		print(street +' '+addres)
-
-		#tmp=re.findall(r'(?:,?[^,]+,?){1}?', addres)
-		#tmp=re.split(r',', addres)
-
-		tmp=re.findall(r',? ?ул\w*[ \.][^,]*,?', addres)
-		tmp2=re.findall(r',?[^,]+ ул\w*,?', addres)
-		if len(tmp)==0 and len(tmp2)==0:
-			for street in list_streets:
-				if addres.find(street.lower().strip())!=-1:
-					print(street.strip() +' '+addres)
-			
-		else:
-			if len(tmp2)==0:
-				pass
-				#print(tmp)
-			if len(tmp)==0:
-				pass
-				#print(tmp2)
-
 
 def find_street_name_from_file(path_address,path_all_streets_file):
 	all_streets = open(path_all_streets_file,'r')
@@ -153,29 +264,4 @@ def find_street_name_from_file(path_address,path_all_streets_file):
 	print('found '+str(count_found))
 	print('repeat '+str(count_overlap2))
 	print('not found '+str(count_not_found))
-
-		
-
-
-
-
-#path_file = 'C:\\v.orlov\\Programm\\python\\AvitoParser\\YearOfConstruction\\address.txt'
-#path_all_streets='C:\\v.orlov\\Programm\\python\\AvitoParser\\YearOfConstruction\\streets_list.txt'
-#file = open(path_file,'r')
-#all_streets = open(path_all_streets,'r')
-
-main_path=sys.path[0]
-file_name_address = 'address.txt'
-file_name_streets='streets_list.txt'
-#address = open(main_path+'\\'+file_name_address,'r')
-#streets = open(main_path+'\\'+file_name_streets,'r')
-
-
-#find_regular_word(path_file)
-#find_street_name_from_file(main_path+'\\'+file_name_address,main_path+'\\'+file_name_streets)
-find_regular_street(main_path+'\\'+file_name_address,main_path+'\\'+file_name_streets)
-
-
-
-input()
 
